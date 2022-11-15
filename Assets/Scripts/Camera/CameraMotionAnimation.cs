0d043a9d73efb9cc0db1927ea36c781164cb7126
Andrey Hoderev <HoderevAV@gmail.com>
2022-11-15 08:59:08 +0400

    Added new virtual camera

--- Assets/Scripts/Camera/CameraMotionAnimation.cs
+++ Assets/Scripts/Camera/CameraMotionAnimation.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.Events;
 
 public class CameraMotionAnimation : MonoBehaviour
 {
     [SerializeField] private Transform _targetPosition;
     [SerializeField] private float _duration;
 
+    private Tween _tween;
+
+    private bool _isPlaying => _tween.IsActive();
+
+    public event UnityAction Played;
+
+    private void Update()
+    {
+        if (_isPlaying == false)
+            Played?.Invoke();
+    }
+
     private void OnEnable()
     {
-        transform.DOMove(_targetPosition.position, _duration);
+        _tween = transform.DOMove(_targetPosition.position, _duration);
     }
 }
