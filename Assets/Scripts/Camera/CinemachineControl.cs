7c29e73b6359b8817eb658e05e97421826f4dc48
Andrey Hoderev <HoderevAV@gmail.com>
2022-11-18 11:37:37 +0400

    Added admin panel

--- Assets/Scripts/Camera/CinemachineControl.cs
+++ Assets/Scripts/Camera/CinemachineControl.cs
@@ -7,33 +7,17 @@ public class CinemachineControl: MonoBehaviour
     [SerializeField] private GameObject _virtualCamera1;
     [SerializeField] private GameObject _virtualCamera2;
     [SerializeField] private GameObject _virtualCamera3;
-    [SerializeField] private GameObject _virtualCamera4;
-
-    private CameraMotionAnimation _cameraMotionAnimation;
-
-    private void Awake()
-    {
-        if(_virtualCamera4.TryGetComponent(out CameraMotionAnimation cameraMotionAnimation))
-            _cameraMotionAnimation = cameraMotionAnimation;
-
-        if (_cameraMotionAnimation != null)
-            _virtualCamera4.SetActive(true);
-        else
-            _virtualCamera1.SetActive(true);
-    }
 
     private void OnEnable()
     {
         _dominoPlace.AllShowed += OnAllShowed;
         _lastDomino.Finished += OnFinished;
-        _cameraMotionAnimation.Played += OnPlayed;
     }
 
     private void OnDisable()
     {
         _dominoPlace.AllShowed -= OnAllShowed;
         _lastDomino.Finished -= OnFinished;
-        _cameraMotionAnimation.Played += OnPlayed;
     }
 
     private void OnAllShowed()
@@ -46,11 +30,5 @@ public class CinemachineControl: MonoBehaviour
     {
         _virtualCamera2.SetActive(false);
         _virtualCamera3.SetActive(true);
-    }
-    
-    private void OnPlayed()
-    {
-        _virtualCamera4.SetActive(false);
-        _virtualCamera1.SetActive(true);
     }
 }
